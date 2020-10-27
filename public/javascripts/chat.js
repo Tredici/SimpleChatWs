d5615e2f4ee86c7a9fda292476109591026ad628
tredici <m.mangiacapre@santannapisa.it>
2020-10-27 15:53:57 +0100

    Aggiunta possibilità allegare file, .gitignore.
    Ora è possibile allegare un numero arbitrario di file ai propri messaggi.
    Ho aggiunto .gitignore ai file tracciati da git.
    Ho migliorato l'aspetto grafico dei file allegati.

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -6,6 +6,7 @@ const socket = io()
  * @param {object} msg 
  */
 function addMsg(msg) {
+    // Creazione del contenitore
     let divMsg = document.createElement("div")
     divMsg.classList.add("msg", "border", "rounded", 
         "mb-2", "bg-primary", "text-white", "p-2")
@@ -13,11 +14,36 @@ function addMsg(msg) {
     let name = document.createElement("p")
     name.classList.add("font-weight-bold", "msg-content", "mb-1")
     name.textContent = msg.auth
-    let content = document.createElement("p")
-    content.classList.add("text-justify", "msg-content", "mb-0")
-    content.textContent = msg.msg.text
+    divMsg.append(name)
 
-    divMsg.append(name,content)
+    // contenuto testuale
+    if(msg.msg.text) {
+        let content = document.createElement("p")
+        content.classList.add("text-justify", "msg-content", "mb-0")
+        content.textContent = msg.msg.text
+        divMsg.append(content)
+    }
+    // file allegati
+    if(msg.msg.files) {
+        let fileList = document.createElement("ul")
+        fileList.classList.add("list-group", "m-0", "font-italic")
+        for(let file of msg.msg.files) {
+            let li = document.createElement("li")
+            li.classList.add("list-group-item", "pt-0", "pb-0")
+            let a = document.createElement("a")
+            //a.classList.add("text-white")
+            li.append(a)
+            
+            let newFile = new File(file.content, file.name)
+            a.href = URL.createObjectURL(newFile)
+            a.textContent = file.name
+            a.download = ""
+
+            fileList.append(li)
+        }
+        divMsg.append(fileList)
+    }
+
     
     return divMsg
 }
@@ -51,13 +77,39 @@ $(
                 /**
                  * Codice per l'upload di nuovi messaggi
                  */
-                $('#chat-form').submit(e => {
+                $('#chat-form').submit(async e => {
                     e.preventDefault()
                     let form = e.currentTarget
                     //let data = new FormData(form)
                     let text_msg = $('#text-msg').val().trim()
                     let data = {
                         text: text_msg
+                    }
+                    const filelist = $('#file-msg')[0].files
+                    if(filelist.length) {
+                        data.files = []
+                        for(let fileToRead of filelist) {
+                            let reader = new FileReader()
+                            let fileBuffer = await new Promise((resolve, reject) => {
+                                reader.onload = function(evt) {
+                                    resolve([evt.target.result])
+                                }
+                                reader.onerror = reject
+                                reader.readAsArrayBuffer(fileToRead)
+                            })
+                            const file = {
+                                name: fileToRead.name,
+                                type: fileToRead.type,
+                                content: fileBuffer
+                            }
+                            /**
+                             * Aggiunge alla risposta il file appena caricato,
+                             * mi tengo tranquillo per un eventuale di invio di
+                             * più file insieme
+                             */
+                            data.files.push(file)
+                        }
+
                     }
                     socket.emit("msg", data)
                     form.reset()
