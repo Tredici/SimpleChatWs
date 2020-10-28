0b955673c7ea44246b2958890fca746f563945ff
tredici <m.mangiacapre@santannapisa.it>
2020-10-28 16:13:11 +0100

    Suddiviso file js. Migliorie estetiche alla pagina.
    Ho suddiviso il file js "public/javascripts/chat.js" in due file aggiungendo "public/javascripts/msgUtils.js" per contenere il solo codice js relativo all'aggiunta di nuovi messaggi.
    Ho modificato l'estetica della pagina facendo sì che l'elenco degli utenti connessi e il form per inserire i dati siano sempre visibili nonostante lo scrol.
    Per quanto riguarda il form l'ho modificato per far sì che non comparisse più l'inutile spazio bianco (dovuto a una label non usata) sopra l'elemento textarea per il testo.
    Ho inoltre aggiunto un po' di padding sopra la form e del margin sotto i campi files e audio (per l'audio registrato) nei messaggi mostrati nella chat.

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,60 +1,6 @@
 const data = {}
 const socket = io()
 
-/** Crea un div che rappresenta il messaggio appena ricevuto
- * 
- * @param {object} msg 
- */
-function addMsg(msg) {
-    // Creazione del contenitore
-    let divMsg = document.createElement("div")
-    divMsg.classList.add("msg", "border", "rounded", 
-        "mb-2", "bg-primary", "text-white", "p-2")
-
-    let name = document.createElement("p")
-    name.classList.add("font-weight-bold", "msg-content", "mb-1")
-    name.textContent = msg.auth
-    divMsg.append(name)
-
-    // contenuto testuale
-    if(msg.msg.text) {
-        let content = document.createElement("p")
-        content.classList.add("text-justify", "msg-content", "mb-0")
-        content.textContent = msg.msg.text
-        divMsg.append(content)
-    }
-    // file allegati
-    if(msg.msg.files) {
-        let fileList = document.createElement("ul")
-        fileList.classList.add("list-group", "m-0", "font-italic")
-        for(let file of msg.msg.files) {
-            let li = document.createElement("li")
-            li.classList.add("list-group-item", "pt-0", "pb-0")
-            let a = document.createElement("a")
-            //a.classList.add("text-white")
-            li.append(a)
-            
-            let newFile = new File(file.content, file.name)
-            a.href = URL.createObjectURL(newFile)
-            a.textContent = file.name
-            a.download = file.name
-
-            fileList.append(li)
-        }
-        divMsg.append(fileList)
-    }
-    // audio
-    if(msg.msg.audio) {
-        let audio = document.createElement("audio")
-        let audioBlob = new Blob(msg.msg.audio)
-        audio.src = URL.createObjectURL(audioBlob)
-        audio.controls = true
-        divMsg.append(audio)
-    }
-
-    
-    return divMsg
-}
 let audio_msg = undefined
 $(
     async () => {
---
+++ public/javascripts/msgUtils.js
@@ -0,0 +1,60 @@
+"use strict"
+
+/** In questo file saranno raccolte le utility per gestire
+ *  la creazione di nuovi messaggi   
+ */
+
+/** Crea un div che rappresenta il messaggio appena ricevuto
+ * 
+ * @param {object} msg 
+ */
+function addMsg(msg) {
+    // Creazione del contenitore
+    let divMsg = document.createElement("div")
+    divMsg.classList.add("msg", "border", "rounded", 
+        "mb-2", "bg-primary", "text-white", "p-2")
+
+    let name = document.createElement("p")
+    name.classList.add("font-weight-bold", "msg-content", "mb-1")
+    name.textContent = msg.auth
+    divMsg.append(name)
+
+    // contenuto testuale
+    if(msg.msg.text) {
+        let content = document.createElement("p")
+        content.classList.add("text-justify", "msg-content", "mb-0")
+        content.textContent = msg.msg.text
+        divMsg.append(content)
+    }
+    // file allegati
+    if(msg.msg.files) {
+        let fileList = document.createElement("ul")
+        fileList.classList.add("list-group", "m-0", "font-italic", "mb-1")
+        for(let file of msg.msg.files) {
+            let li = document.createElement("li")
+            li.classList.add("list-group-item", "pt-0", "pb-0")
+            let a = document.createElement("a")
+            //a.classList.add("text-white")
+            li.append(a)
+            
+            let newFile = new File(file.content, file.name)
+            a.href = URL.createObjectURL(newFile)
+            a.textContent = file.name
+            a.download = file.name
+
+            fileList.append(li)
+        }
+        divMsg.append(fileList)
+    }
+    // audio
+    if(msg.msg.audio) {
+        let audio = document.createElement("audio")
+        audio.classList.add("mb-1")
+        let audioBlob = new Blob(msg.msg.audio)
+        audio.src = URL.createObjectURL(audioBlob)
+        audio.controls = true
+        divMsg.append(audio)
+    }
+    
+    return divMsg
+}
