fe72e4ff28acc780f1db3f555be7257711721878
tredici <m.mangiacapre@santannapisa.it>
2020-10-30 17:15:24 +0100

    Preparato il branch per i test per le nuove features.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var sassMiddleware = require('node-sass-middleware');
 //var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
 var chatRouter = require('./routes/chat');
+var testsRouter = require('./routes/tests');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', chatRouter);
+app.use('/tests', testsRouter);
 //app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 
