14e153cc66e4cbf79f863f1df72caadf3563491d
tredici <m.mangiacapre@santannapisa.it>
2020-11-02 17:56:49 +0100

    Testata possibilità di streaming schermo.
    Scoperto che è effettivamente possibile registrare il proprio schermo con semplicità.
    Questa informazione sarà usata più avanti.

--- routes/tests.js
+++ routes/tests.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render("video", {title:"Test Video"});
 });
 
+router.get('/screen', function(req, res, next) {
+  res.render("screen", {title:"Test Schermo"});
+});
+
+
 module.exports = router;
